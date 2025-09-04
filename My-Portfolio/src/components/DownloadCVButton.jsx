a2931509727177b105d6e2b278c55881709c6169
0shuaibshaikh <mshuaib279@gmail.com>
2025-09-04 23:24:38 +0530

    Updated project files

--- My-Portfolio/src/components/DownloadCVButton.jsx
+++ My-Portfolio/src/components/DownloadCVButton.jsx
@@ -4,7 +4,7 @@ const DownloadCVButton = () => {
   return (
     <div className="flex justify-center items-center mt-6 mb-6">
       <a
-        href="/shuaib Resume.pdf"
+        href="/shuaib_resume.pdf"
         download="shuaib_Resume.pdf"
         className="bg-blue-600 text-white font-semibold px-6 py-3 rounded-full shadow-lg hover:bg-blue-800 hover:shadow-xl hover:scale-105 transition-all duration-300 ease-in-out text-lg uppercase"
       >
